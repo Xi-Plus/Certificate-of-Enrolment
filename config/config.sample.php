2a4485a03336e370e5226dd2f5e1ca4944cd822f
Xi-Plus <huangxuanyuxiplus@gmail.com>
2022-08-23 11:39:55 +0800

    Add text/csv

--- config/config.sample.php
+++ config/config.sample.php
@@ -11,7 +11,7 @@ $C["DBpass"] = '';
 $C["DBname"] = 'certificate_of_enrolment';
 
 $C["cookiename"] = 'coe';
-$C["cookieexpire"] = 86400*7;
+$C["cookieexpire"] = 86400 * 7;
 
 $C["CAPTCHAuselogin"] = false;
 $C["CAPTCHAusestudent"] = false;
@@ -22,19 +22,19 @@ $C["PasswordSecurityEnabled"] = true;
 $C["PasswordSecurityMinLength"] = 4;
 $G["PasswordSecurityText"] = [
 	"password_match_username" => "密碼與帳號相同",
-	"password_too_short" => "密碼太短，至少要".$C["PasswordSecurityMinLength"]."個字",
-	"password_is_popular" => "密碼在常見密碼列表中前".$C["PasswordSecurityCannotBePopular"]."位"
+	"password_too_short" => "密碼太短，至少要" . $C["PasswordSecurityMinLength"] . "個字",
+	"password_is_popular" => "密碼在常見密碼列表中前" . $C["PasswordSecurityCannotBePopular"] . "位"
 ];
 
-$G["db"] = new PDO ('mysql:host='.$C["DBhost"].';dbname='.$C["DBname"].';charset=utf8', $C["DBuser"], $C["DBpass"]);
+$G["db"] = new PDO('mysql:host=' . $C["DBhost"] . ';dbname=' . $C["DBname"] . ';charset=utf8', $C["DBuser"], $C["DBpass"]);
 
 $C["function"] = [];
-$C["function"]["print-date"] = function() {
+$C["function"]["print-date"] = function () {
 	return date("Y.m.d");
 };
 
-$G["csvmime"] = array("application/vnd.ms-excel", "text/comma-separated-values");
-$G["pdfmime"] = array("application/pdf");
+$G["csvmime"] = ["application/vnd.ms-excel", "text/comma-separated-values", "text/csv"];
+$G["pdfmime"] = ["application/pdf"];
 $G["input_type"] = [
 	"authentication" => "驗證用",
 	"custom" => "自行輸入"
@@ -49,7 +49,7 @@ $G["source_text"] = [
 $G['safety_check_student'] = [];
 $G['safety_check_school'] = [];
 
-$G['BOM'] = chr(239).chr(187).chr(191);
+$G['BOM'] = chr(239) . chr(187) . chr(191);
 
 date_default_timezone_set("Asia/Taipei");
 
